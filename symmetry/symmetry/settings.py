7f4e002335d26b0f33cf002cc764aac99591012b
Tkalich A <anton.tkalich@yandex.ru>
2022-03-03 22:18:04 +0300

    03.03

--- symmetry/symmetry/settings.py
+++ symmetry/symmetry/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'symmetry.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'symmetry',
+        'USER': 'admin',
+        'PASSWORD': 'A54182',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
